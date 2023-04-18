1ad5f303da65db7880be25843ae487b784f87656
Roberto Sanchez <59806324+rsanc1103@users.noreply.github.com>
2023-04-18 16:01:23 -0600

    testing submitting new games sto ontology

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from owlready2 import *
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     onto = get_ontology("/Users/robertosanchez/Desktop/gaming_popularity.owl").load()
     Game_class = onto.Game
     new_game = onto.Game()
@@ -16,8 +16,18 @@ def hello():
     ranks = onto.search(is_a = onto.Rank)
     ratings = onto.search(is_a = onto.Rating)
 
-    # return 'Hello world'
     return render_template("index.html", cl=cl, games=games, ranks=ranks, ratings=ratings)
+
+@app.route('/save', methods=['POST'])
+def save():
+    onto = get_ontology("/Users/robertosanchez/Desktop/gaming_popularity.owl").load()
+    gameName = request.form.get('game_name')
+    new_game = onto.Game()
+    new_game.name = gameName
+
+    return home()
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
