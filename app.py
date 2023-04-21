5710b753b3ad6cb808d20f2e70c28a81741a46c7
Roberto Sanchez <59806324+rsanc1103@users.noreply.github.com>
2023-04-21 14:16:22 -0600

    trying different queries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import *
 from owlready2 import *
 
 
@@ -7,8 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     onto = get_ontology("/Users/robertosanchez/Desktop/gaming_popularity.owl").load()
-    Game_class = onto.Game
-    new_game = onto.Game()
 
     cl = list(onto.classes())
 
@@ -16,16 +14,49 @@ def home():
     ranks = onto.search(is_a = onto.Rank)
     ratings = onto.search(is_a = onto.Rating)
 
-    return render_template("index.html", cl=cl, games=games, ranks=ranks, ratings=ratings)
+    objectProperties = onto.object_properties()
+
+    q = list(default_world.sparql("""
+                            SELECT ?game
+                            { ?game rdf:type owl:Class. }
+            """))
+    print(q)
+
+
+
+    with onto:
+        class Game(Thing):
+            pass
+        class Rating(Thing):
+            pass
+        class hasRating(ObjectProperty):
+            domain = [Game]
+            range = [str]
+
+            for i in list(Rating.instances()):
+                print(i.name)
+
+            # Query
+            # Game that hasRating value M
+            results = list(onto.search(type=Game, hasRating=onto.E))
+            for r in results:
+                print(r.name)
+                
+            print(list(onto.object_properties()))
+
+    return render_template("index.html", cl=cl, games=games, ranks=ranks, ratings=ratings, objectProperties=objectProperties)
 
 @app.route('/save', methods=['POST'])
 def save():
     onto = get_ontology("/Users/robertosanchez/Desktop/gaming_popularity.owl").load()
     gameName = request.form.get('game_name')
     new_game = onto.Game()
-    new_game.name = gameName
+    new_game.name = gameName.replace(' ', '_')
 
-    return home()
+    onto.save(file="/Users/robertosanchez/Desktop/gaming_popularity.owl", format="rdfxml")
+
+
+    return redirect(url_for('home'))
 
 
 
