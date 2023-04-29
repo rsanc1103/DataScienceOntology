50e5021aec49b92c3d0fa958caaca71ea29ad013
Roberto Sanchez <59806324+rsanc1103@users.noreply.github.com>
2023-04-28 21:04:18 -0600

    updated ontology and attributions

--- app.py
+++ app.py
@@ -14,14 +14,14 @@ def home():
     onto = get_ontology("gaming_popularity.owl").load()
     cl = list(onto.classes())
     games = onto.search(is_a = onto.Game)
-    ranks = onto.search(is_a = onto.Rank)
     ratings = onto.search(is_a = onto.Rating)
+    genres = onto.search(is_a = onto.Genre)
     objectProperties = list(onto.object_properties())
     dataProperties = list(onto.data_properties())
 
     results = result
 
-    return render_template("index.html", cl=cl, games=games, ranks=ranks, ratings=ratings, objectProperties=objectProperties, dataProperties=dataProperties, results=results)
+    return render_template("index.html", cl=cl, games=games,genres=genres, ratings=ratings, objectProperties=objectProperties, dataProperties=dataProperties, results=results)
 
 @app.route('/clear', methods=['POST'])
 def clear():
@@ -32,65 +32,18 @@ def clear():
 
 @app.route('/query', methods=['POST'])
 def query():
-    # if request.form.get('q_class') == 'Game':
-    #     query = """
-    #                 PREFIX onto:<http://www.semanticweb.org/robertosanchez/ontologies/2023/2/gaming_popularity#>
-    #                 SELECT ?class
-    #                 WHERE {{
-    #                     ?class rdf:type onto:{} .
-    #                     ?class onto:{} onto:{} .
-    #                 }}
-    #     """.format(request.form.get('q_class'), request.form.get('objectProperty'), request.form.get('q_rating'))
-
-    #     if request.form.get('q_class') == 'Game' and request.form.get('objectProperty') == 'isRanked' or request.form.get('objectProperty') == 'genre':
-    #         query = """
-    #                     PREFIX onto:<http://www.semanticweb.org/robertosanchez/ontologies/2023/2/gaming_popularity#>
-    #                     SELECT ?class
-    #                     WHERE {{
-    #                         ?class rdf:type onto:{} .
-    #                         ?class onto:{} "{}" .
-    #                     }}
-    #         """.format(request.form.get('q_class'), request.form.get('objectProperty'), request.form.get('q_rank'))
-    #         print(query)
-    #     qres = list(default_world.sparql(query))
-    # elif request.form.get('q_class') == 'Rating':
-    #     query = """
-    #                     PREFIX onto:<http://www.semanticweb.org/robertosanchez/ontologies/2023/2/gaming_popularity#>
-    #                     SELECT ?class
-    #                     WHERE {{
-    #                         ?class rdf:type onto:{} .
-    #                     }}
-    #     """.format(request.form.get('q_class'))
-    #     qres = list(default_world.sparql(query))
-    # elif request.form.get('q_class') == 'Game' and request.form.get('objectProperty') == 'isRanked':
-    #     query = """
-    #                     PREFIX onto:<http://www.semanticweb.org/robertosanchez/ontologies/2023/2/gaming_popularity#>
-    #                     SELECT ?class
-    #                     WHERE {{
-    #                         ?class rdf:type onto:{} .
-    #                         ?class onto:{} "{}"^^xsd:integer .
-    #                     }}
-    #     """.format(request.form.get('q_class'), request.form.get('objectProperty'), request.form.get('q_rank'))
-        
 
     query = """
             PREFIX onto:<http://www.semanticweb.org/robertosanchez/ontologies/2023/2/gaming_popularity#>
             """
     query += request.form.get('sparql')
-
-    print(query)
-
     qres = list(default_world.sparql(query))
-    print((str)(qres))
+
     global result
     result = qres
 
     return redirect(url_for('home'))
 
-# SELECT ?game
-# WHERE{{
-# ?game rdf:type onto:Game.
-# }}
 
 
 if __name__ == '__main__':
